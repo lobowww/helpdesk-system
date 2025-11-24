47a8c89fef85c6e2584a03b8e27b4b6e12471656
Felipe Lobo <felipelboss@gmail.com>
2025-11-23 21:23:32 -0300

    create para Chamado

--- src/main/java/com/felipe/helpdesk/Services/ChamadoService.java
+++ src/main/java/com/felipe/helpdesk/Services/ChamadoService.java
@@ -8,13 +8,26 @@ import org.springframework.stereotype.Service;
 
 import com.felipe.helpdesk.Services.exceptions.ObjectnotFoundException;
 import com.felipe.helpdesk.domain.Chamado;
+import com.felipe.helpdesk.domain.Cliente;
+import com.felipe.helpdesk.domain.Tecnico;
+import com.felipe.helpdesk.domain.dtos.ChamadoDTO;
+import com.felipe.helpdesk.domain.enums.Prioridade;
+import com.felipe.helpdesk.domain.enums.Status;
 import com.felipe.helpdesk.repositories.ChamadoRepository;
+
+import jakarta.validation.Valid;
 
 @Service
 public class ChamadoService {
 	
 	@Autowired
 	private ChamadoRepository repository;//Faz a comunicação com o banco
+	
+	@Autowired
+	private TecnicoService tecnicoService;
+	
+	@Autowired
+	private ClienteService clienteService;
 	
 	public Chamado findById(Integer id) {
 		Optional<Chamado> obj = repository.findById(id);
@@ -23,5 +36,27 @@ public class ChamadoService {
 	
 	public List<Chamado> findAll(){
 		return repository.findAll();
+	}
+
+	public Chamado create(@Valid ChamadoDTO objDTO) {
+		return repository.save(newChamado(objDTO));//Com o chamado criado salvo ele no banco
+	}
+	
+	private Chamado newChamado(ChamadoDTO obj) {//Recebi um chamado DTO e a partir dele começo a construir o meu chamado que vai ser salvo no banco
+		Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
+		Cliente cliente = clienteService.findById(obj.getCliente());
+		
+		Chamado chamado = new Chamado();
+		if(obj.getId() != null) {
+			chamado.setId(obj.getId());
+		}
+		
+		chamado.setTecnico(tecnico);
+		chamado.setCliente(cliente);
+		chamado.setPrioridade(Prioridade.toEnum(obj.getPrioridade()));
+		chamado.setStatus(Status.toEnum(obj.getStatus()));
+		chamado.setTitulo(obj.getTitulo());
+		chamado.setObservacoes(obj.getObservacoes());
+		return chamado;
 	}
 }
--- src/main/java/com/felipe/helpdesk/domain/dtos/ChamadoDTO.java
+++ src/main/java/com/felipe/helpdesk/domain/dtos/ChamadoDTO.java
@@ -6,6 +6,8 @@ import java.time.LocalDate;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.felipe.helpdesk.domain.Chamado;
 
+import jakarta.validation.constraints.NotNull;
+
 public class ChamadoDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
@@ -14,12 +16,18 @@ public class ChamadoDTO implements Serializable{
     private LocalDate dataAbertura = LocalDate.now();
 	@JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate dataFechamento;
+	@NotNull(message = "O campo PRIORIDADE é requerido")
     private Integer prioridade;//O mesmo do tecnico serve para prioridade
-    private Integer status;//O mesmo do tecnico serve para status
-    private String titulo;
-    private String observacoes;
-    private Integer tecnico;//private Tecnico tecnico; -> Não precisamos de todas as informações do Tecnico no chamado, somente do id
-    private Integer cliente;//O mesmo do tecnico serve para o cliente
+	@NotNull(message = "O campo STATUS é requerido")
+	private Integer status;//O mesmo do tecnico serve para status
+	@NotNull(message = "O campo TITULO é requerido")
+	private String titulo;
+	@NotNull(message = "O campo OBSERVAÇÕES é requerido")
+	private String observacoes;
+	@NotNull(message = "O campo TECNICO é requerido")
+	private Integer tecnico;//private Tecnico tecnico; -> Não precisamos de todas as informações do Tecnico no chamado, somente do id
+	@NotNull(message = "O campo CLIENTE é requerido")
+	private Integer cliente;//O mesmo do tecnico serve para o cliente
     private String nomeTecnico;// No chamado vamos ter uma coluna com o nome do Tecnico e o nome do Cliente, logo se eu quero pegar as informações
     //De um Tecnico eu faço um FindById do id do mesmo e puxo as informações e mostro na tabela. Só que imagine que eu tenho 50 chamados no meu banco
     //Tenho que fazer 1 FindById para cada chamado para pegar o nome do Tecnico, deixando nada permormático o nosso código. Para evitar isso vamos criar o nomeTecnico.
